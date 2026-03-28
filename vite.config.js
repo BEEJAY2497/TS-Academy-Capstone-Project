30e1c605d1b923977b439f414e2f313df8885dbf
Beejay2497 <Bolaji6718@gmail.com>
2026-03-28 18:04:51 -0500

    Update project

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Determine if we are running a GitHub Pages deploy
+const isGitHubPages = process.env.DEPLOY_ENV === 'GH_PAGES';
+
 export default defineConfig({
   plugins: [react()],
-  base: '/TS-Academy-Capstone-Project/'
+  base: isGitHubPages
+    ? '/TS-Academy-Capstone-Project/' // for GitHub Pages
+    : './' // for local testing
 });
